815046847d9b7c9c332662760640834035cae56a
Gregory Albouy <60700958+GregoryAlbouy@users.noreply.github.com>
2021-09-10 15:28:22 +0200

    Implement Pascal (#2)
    * Implement Pascal
    
    * Write unit tests for Pascal

--- strcase.go
+++ strcase.go
@@ -1,33 +1,13 @@
 package strcase
 
-import (
-	"unicode"
-)
-
+// Camel returns a camel-cased version of the input string.
+// It currently only supports conversions from pascal case.
 func Camel(in string) string {
-	cc := []rune{}
-	ok := false
-	for i, r := range in {
-		switch {
-		case r == '_':
-			continue
-		case i == len(in)-1:
-			doAppend(&cc, r, !ok)
-		case ok:
-			doAppend(&cc, r, false)
-		case unicode.IsLower(rune(in[i+1])):
-			doAppend(&cc, r, i == 0)
-			ok = true
-		default:
-			doAppend(&cc, r, true)
-		}
-	}
-	return string(cc)
+	return camel(in)
 }
 
-func doAppend(dst *[]rune, r rune, lower bool) {
-	if lower {
-		r = unicode.ToLower(r)
-	}
-	*dst = append(*dst, r)
+// Pascal returns a pascal-cased version of the input string.
+// It currently only supports conversions from camel case.
+func Pascal(in string) string {
+	return pascal(in)
 }
